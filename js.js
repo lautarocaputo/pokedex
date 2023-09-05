d0bee9d8a2add4d61b6f91a99bf121c7fe349b8e
LautaroC <lautaro0611@gmail.com>
2023-09-05 15:19:11 -0300

    doble tipo

--- js.js
+++ js.js
@@ -2,7 +2,9 @@ const listaPokemon = $("#listaPokemon");
 const tipos = $(".btn-header");
 let url = "https://pokeapi.co/api/v2/pokemon/";
 
-for (let i = 1; i <= 200; i++) {
+cantidadPokemons = 151;
+
+for (let i = 1; i <= cantidadPokemons; i++) {
   fetch(url + i)
     .then((response) => response.json())
     .then((data) => mostrarPokemon(data));
@@ -43,27 +45,70 @@ function mostrarPokemon(data) {
   listaPokemon.append(div);
 }
 
+let tiposSeleccionados = [];
+
 tipos.on("click", function (event) {
-  const botonId = event.currentTarget.id; // Obtenemos el ID del botón clicado
+  const botonId = event.currentTarget.id;
+  const boton = $(event.currentTarget);
+  console.log(boton);
+  // Verificar si el botón clickeado es "ver-todos"
+  if (botonId === "ver-todos") {
+    tiposSeleccionados = []; // Reiniciar los tipos seleccionados
+    tipos.removeClass("button-clicked");
+  } else {
+    boton.addClass("button-clicked");
+    // Si el botón clickeado es un tipo, agregarlo o eliminarlo de la lista según su estado actual
+    if (tiposSeleccionados.includes(botonId)) {
+      // Si ya está en la lista, quitarlo
+      tiposSeleccionados = tiposSeleccionados.filter((tipo) => tipo !== botonId);
+      boton.removeClass("button-clicked");
+    } else {
+      // Si no está en la lista, agregarlo
+      tiposSeleccionados.push(botonId);
+    }
+  }
 
-  listaPokemon.empty(); // Vaciamos la lista de Pokémon antes de agregar nuevos resultados
+  listaPokemon.empty();
 
-  for (let i = 1; i <= 200; i++) {
+  for (let i = 1; i <= cantidadPokemons; i++) {
     $.ajax({
       url: url + i,
       dataType: "json",
       success: function (data) {
-        if (botonId === "ver-todos") {
+        const tipos = data.types.map((type) => type.type.name);
+
+        // Verificar si se deben mostrar Pokémon basados en los tipos seleccionados
+        if (
+          tiposSeleccionados.length === 0 || // Si no se han seleccionado tipos, mostrar todos
+          tiposSeleccionados.every((tipo) => tipos.includes(tipo))
+        ) {
           mostrarPokemon(data);
-        } else {
-          const tipos = data.types.map((type) => type.type.name);
-          if (tipos.some((tipo) => tipo.includes(botonId))) {
-            mostrarPokemon(data);
-          }
         }
       },
       error: function () {
         // Manejar errores aquí si es necesario
+      },
+    });
+  }
+});
+
+$("#btn-buscar").on("click", function (event) {
+  event.preventDefault();
+  let busqueda = $("#input-buscar").val().toLowerCase();
+  listaPokemon.empty();
+
+  for (let i = 1; i <= cantidadPokemons; i++) {
+    $.ajax({
+      url: url + i,
+      dataType: "json",
+      success: function (data) {
+        if (data.name.includes(busqueda)) {
+          mostrarPokemon(data);
+          $("#input-buscar").val("");
+        }
+      },
+      error: function () {
+
       },
     });
   }
